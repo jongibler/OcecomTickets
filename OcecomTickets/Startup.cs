bf849408a438ada1cd262b789863231ce633440a
Jonathan <jgibler@gmail.com>
2017-09-14 12:27:16 -0500

    Add project files.

---
+++ OcecomTickets/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(OcecomTickets.Startup))]
+namespace OcecomTickets
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
