744256942bf96b738152da646759d41b7e42b602
Jonathan <jgibler@gmail.com>
2017-09-14 15:33:38 -0500

    Create clients. User email validation.

---
+++ OcecomTickets/Controllers/ClientsController.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OcecomTickets.Models;
+
+namespace OcecomTickets.Controllers
+{
+    [Authorize(Roles ="Admin")]
+    public class ClientsController : Controller
+    {
+        private OcecomTicketsContext db = new OcecomTicketsContext();
+
+        // GET: Clients
+        public ActionResult Index()
+        {
+            return View(db.Clients.ToList());
+        }
+
+        // GET: Clients/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Clients/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(Client client)
+        {
+            if (UserEmailExists(client.Email))
+            {
+                ModelState.AddModelError("Email", "El correo ya existe para otro usuario.");
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Clients.Add(client);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(client);
+        }
+
+        private bool UserEmailExists(string email)
+        {
+            using (var usersDB = new ApplicationDbContext())
+            {
+                return usersDB.Users.Any(u => u.UserName.ToLower() == email.ToLower());
+            }           
+        }
+
+        // GET: Clients/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // POST: Clients/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,Email")] Client client)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(client).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(client);
+        }
+
+        // GET: Clients/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Client client = db.Clients.Find(id);
+            if (client == null)
+            {
+                return HttpNotFound();
+            }
+            return View(client);
+        }
+
+        // POST: Clients/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Client client = db.Clients.Find(id);
+            db.Clients.Remove(client);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OcecomTickets/Models/Client.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace OcecomTickets.Models
+{
+    public class Client
+    {
+        public int Id { get; set; }
+
+        [Required(ErrorMessage ="Requerido")]        
+        public string ClientName { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public string ContactName { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        [EmailAddress(ErrorMessage ="Inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public string OfficePhone { get; set; }
+
+        public string MobilePhone { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public int MonthlyHours { get; set; }
+
+        [DataType(DataType.MultilineText)]
+        public string Notes { get; set; }
+    }
+}
