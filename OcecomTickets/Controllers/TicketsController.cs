a59fe7b9d013821b63cbc01964ce2e4ab493fc1e
Jonathan <jgibler@gmail.com>
2017-09-17 14:28:10 -0500

    Create tickets. Show tickets for client.

---
+++ OcecomTickets/Controllers/TicketsController.cs
@@ -0,0 +1,143 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using OcecomTickets.Models;
+using Microsoft.AspNet.Identity.Owin;
+
+namespace OcecomTickets.Controllers
+{
+    [Authorize]
+    public class TicketsController : Controller
+    {
+        private OcecomTicketsContext db = new OcecomTicketsContext();
+
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            if (User.IsInRole("Client"))
+            {
+                var clientId = db.Clients.Where(c => c.Email == User.Identity.Name).Select(c => c.Id).First();
+                return View(db.Tickets.Where(t => t.ClientId == clientId).ToList());
+            }
+            else
+            {
+                return View(db.Tickets.ToList());
+            }
+        }
+
+        // GET: Tickets/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // GET: Tickets/Create
+        [Authorize(Roles ="Client")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tickets/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Client")]
+        public ActionResult Create(Ticket ticket)
+        {
+            ticket.CreationDate = DateTime.Now;
+            ticket.Status = "Abierto";
+            ticket.ClientId = db.Clients.Where(c => c.Email == User.Identity.Name).Select(c => c.Id).First();
+
+            if (ModelState.IsValid)
+            {
+                db.Tickets.Add(ticket);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(ticket);
+        }
+
+        // GET: Tickets/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: Tickets/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "Id,CreationDate,Severity,Category,Note")] Ticket ticket)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(ticket).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(ticket);
+        }
+
+        // GET: Tickets/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Ticket ticket = db.Tickets.Find(id);
+            if (ticket == null)
+            {
+                return HttpNotFound();
+            }
+            return View(ticket);
+        }
+
+        // POST: Tickets/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Ticket ticket = db.Tickets.Find(id);
+            db.Tickets.Remove(ticket);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ OcecomTickets/Models/Ticket.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Web;
+
+namespace OcecomTickets.Models
+{
+    public class Ticket
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string Status { get; set; }
+
+        [Required]
+        public DateTime CreationDate { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public string Severity { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+        public string Category { get; set; }
+
+        [Required(ErrorMessage = "Requerido")]
+
+        [DataType(DataType.MultilineText)]
+        public string Note { get; set; }
+
+        public int ClientId { get; set; }
+        public virtual Client Client { get; set; }
+    }
+}
