6f1e2d30af02153592f5246b98a7b1269085905b
Jonathan <jgibler@gmail.com>
2017-09-26 16:57:21 -0500

    empleados - colaboradores text change. reporte clientes demo

---
+++ OcecomTickets/Controllers/ReportsController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OcecomTickets.Controllers
+{
+    [Authorize]
+    public class ReportsController : Controller
+    {
+        [Authorize(Roles ="Client")]
+        public ActionResult Clients()
+        {
+            var ci = new CultureInfo("es-MX");
+            ViewBag.CurrentMonth = DateTime.Today.ToString("MMMM yyyy", ci);
+            return View();
+        }
+    }
+}
